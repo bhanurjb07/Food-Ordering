e1dd41df0e54a8cba3392ec1d809ae2d100d2751
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-10-07 06:52:46 +0530

    commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <img src={assets.logo} alt='logo' />
+      <ul className='navbar-menu'>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-App</li>
+        <li>Contact Us</li>
+      </ul>
     </div>
   )
 }
